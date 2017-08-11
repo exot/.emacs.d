173d4c8caae75c6994fd8968244fbfffa3f7ebe5
Daniel Borchmann <daniel@algebra20.de>
2017-08-12 00:03:26 +0200

    [TeX] Disable ‘electric-pair-mode’

--- site-lisp/db-latex.el
+++ site-lisp/db-latex.el
@@ -35,6 +35,7 @@
 (add-hook 'LaTeX-mode-hook #'outline-minor-mode)
 (add-hook 'LaTeX-mode-hook #'page-break-lines-mode)
 (add-hook 'LaTeX-mode-hook #'turn-on-reftex)
+(add-hook 'LaTeX-mode-hook #'electric-pair-mode)
 
 (put 'TeX-narrow-to-group 'disabled nil)
 (put 'LaTeX-narrow-to-environment 'disabled nil)
