4a0502dbe019acfe6e94bdfb6daca650b9fa5bd5
Daniel Borchmann <daniel@algebra20.de>
2017-09-15 17:12:49 +0200

    [Org] Set values for main org-mode files

--- init.el
+++ init.el
@@ -467,10 +467,16 @@
              db/export-diary
              hydra-org-clock/body)
   :config (progn
-            (require 'db-org)
-
             (setq org-home-task-id "ade37344-ccd9-4c93-aa15-406886a86627"
                   org-working-task-id "a686ba96-116e-457b-94c1-5dbc42f9c051")
+
+            (setq db/org-default-work-file "~/Documents/uni/admin/misc/work.org"
+                  db/org-default-home-file "~/Documents/home/admin/home.org"
+                  db/org-default-notes-file "~/Documents/home/misc/notes.org"
+                  db/org-default-refile-file "~/Desktop/refile.org"
+                  db/org-default-pensieve-file "~/Documents/home/misc/pensieve.org.gpg")
+
+            (require 'db-org)
 
             ;; avoid important buffers to end up in `org-agenda-new-buffers’ by
             ;; opening them manually
