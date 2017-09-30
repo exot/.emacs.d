4ac8be452bb62b7d526d0c4ea6ad2b0fbacd79dd
Daniel Borchmann <daniel@algebra20.de>
2017-09-30 22:40:57 +0200

    [Init] Move ‘pp’ to section of builtin packages

--- init.el
+++ init.el
@@ -487,6 +487,18 @@ _h_   _l_   _o_k        _y_ank
                        (test eq window-system 'x))
                      (cdr (assoc "application" mailcap-mime-data))))))
 
+(use-package pp
+  :commands (db/eval-last-sexp-or-region
+             pp-eval-expression)
+  :config   (progn
+              (defun db/eval-last-sexp-or-region (prefix)
+                ;; http://pages.sachachua.com/.emacs.d/Sacha.html#orgheadline140
+                "Eval region from BEG to END if active, otherwise the last sexp."
+                (interactive "P")
+                (if (and (mark) (use-region-p))
+                    (eval-region (min (point) (mark)) (max (point) (mark)))
+                  (pp-eval-last-sexp prefix)))))
+
 (use-package quail
   :defer t
   :config (progn
@@ -1256,18 +1268,6 @@ _h_   _l_   _o_k        _y_ank
 (use-package page-break-lines
   :commands (page-break-lines-mode)
   :diminish page-break-lines-mode)
-
-(use-package pp
-  :commands (db/eval-last-sexp-or-region
-             pp-eval-expression)
-  :config   (progn
-              (defun db/eval-last-sexp-or-region (prefix)
-                ;; http://pages.sachachua.com/.emacs.d/Sacha.html#orgheadline140
-                "Eval region from BEG to END if active, otherwise the last sexp."
-                (interactive "P")
-                (if (and (mark) (use-region-p))
-                    (eval-region (min (point) (mark)) (max (point) (mark)))
-                  (pp-eval-last-sexp prefix)))))
 
 (use-package python
   :defer t
