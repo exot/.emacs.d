6d4d519aab3d456735e7161c452395fe9d8c1da3
Daniel Borchmann <daniel@algebra20.de>
2017-11-06 21:30:57 +0100

    [Org] Clock time again for taking notes

--- site-lisp/db-org.el
+++ site-lisp/db-org.el
@@ -629,7 +629,8 @@ _y_: ?y? year       _q_: quit          _L__l__c_: ?l?
         ("n" "Note"
              entry
              (file+olp db/org-default-notes-file "Notes")
-             "* %^{About} :NOTE:\n%T\n%?")
+             "* %^{About} :NOTE:\n%T\n%?"
+             :clock-in t :clock-resume t)
         ("d" "Date"
              entry
              (file db/org-default-refile-file)
