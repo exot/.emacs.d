4fa3a7a4d1ca90fbec898b0f918afe158bfe5c8f
Daniel Borchmann <daniel@algebra20.de>
2018-03-03 14:01:41 +0100

    [ELPA] Update

--- elpa/exec-path-from-shell-20180224.1916/exec-path-from-shell-autoloads.el
+++ elpa/exec-path-from-shell-20180224.1916/exec-path-from-shell-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "exec-path-from-shell" "exec-path-from-shell.el"
-;;;;;;  (22911 3932 648589 906000))
+;;;;;;  (23194 22920 277259 350000))
 ;;; Generated autoloads from exec-path-from-shell.el
 
 (autoload 'exec-path-from-shell-copy-envs "exec-path-from-shell" "\
--- elpa/hydra-20180226.1116/hydra-autoloads.el
+++ elpa/hydra-20180226.1116/hydra-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "hydra" "hydra.el" (23179 13926 352819 118000))
+;;;### (autoloads nil "hydra" "hydra.el" (23194 22908 273187 855000))
 ;;; Generated autoloads from hydra.el
 
 (autoload 'defhydra "hydra" "\
@@ -65,7 +65,7 @@ result of `defhydra'.
 ;;;***
 
 ;;;### (autoloads nil nil ("hydra-examples.el" "hydra-ox.el" "hydra-pkg.el"
-;;;;;;  "lv.el") (23179 13926 364819 184000))
+;;;;;;  "lv.el") (23194 22908 285187 927000))
 
 ;;;***
 
