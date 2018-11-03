9f00c24c2832c652292b3eb9dc19a14d2dce3508
Daniel Borchmann <daniel@algebra20.de>
2018-11-03 09:52:38 +0100

    [Org] Remove false autolaod

--- init.el
+++ init.el
@@ -651,7 +651,6 @@ _h_   _l_   _o_k        _y_ank
   :commands (org-agenda
              org-capture
              org-store-link
-             org-clock-save
              hydra-org-clock/body)
   :bind (:map org-mode-map
               ([remap org-return] . org-return-indent))
