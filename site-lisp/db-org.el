f96329c4b65eff72dfeb230d0c99b44e06610bf1
Daniel Borchmann <daniel@algebra20.de>
2018-11-03 09:05:01 +0100

    [Org] Readd accidentally remove hyrda for org mode clocking

--- site-lisp/db-org.el
+++ site-lisp/db-org.el
@@ -789,6 +789,38 @@ In ~%s~:
 (add-hook 'org-mode-hook #'endless/org-ispell)
 
 
+;;; Hydra
+
+(defhydra hydra-org-clock (:color blue)
+  "
+Current Task: %`org-clock-current-task; "
+  ("w" (lambda ()
+         (interactive)
+         (clock-in-task-by-id org-working-task-id)))
+  ("h" (lambda ()
+         (interactive)
+         (clock-in-task-by-id org-home-task-id)))
+  ("b" (lambda ()
+         (interactive)
+         (clock-in-task-by-id org-break-task-id)))
+  ("i" (lambda ()
+         (interactive)
+         (org-clock-in '(4))))
+  ("a" counsel-org-goto-all)
+  ("o" org-clock-out)
+  ("l" db/org-clock-in-last-task)
+  ("p" db/play-playlist)
+  ("d" (lambda ()
+         (interactive)
+         (when (org-clock-is-active)
+           (save-window-excursion
+             (org-clock-goto)
+             (let ((org-inhibit-logging 'note))
+               (org-todo 'done)
+               (org-save-all-org-buffers)))))))
+
+
+
 ;;; Drag-and-Drop images into org-mode buffer
 
 (use-package org-download)
