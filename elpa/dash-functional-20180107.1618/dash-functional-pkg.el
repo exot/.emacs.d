fe391b642a29b25fbedbb708253ebd69c8dea4a1
Daniel Borchmann <daniel@algebra20.de>
2019-12-21 19:10:18 +0100

    Update things from elpa to keep in this repository

--- elpa/dash-functional-20180107.1618/dash-functional-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash-functional" "20180107.1618" "Collection of useful combinators for Emacs Lisp" '((dash "2.0.0") (emacs "24")) :commit "85e8f62b7a8ae0b4da307ddf16e4f1c3559d0d3f" :keywords '("lisp" "functions" "combinators"))
