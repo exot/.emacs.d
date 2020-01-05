66425ee2b7cafb83c4a79712ae8315bebedcf68e
Daniel Borchmann <daniel@algebra20.de>
2020-01-05 13:23:23 +0100

    Add some comments why we don't automatically update `org-agenda-files' anymore

--- init.el
+++ init.el
@@ -957,23 +957,41 @@ With given ARG, display files in `db/important-document-path’."
 
 ;; Agenda
 
+;; NB: some of those files should also be elements of `org-agenda-files', but
+;; this is not done automatically.  The reason is that automatically changing
+;; `org-agenda-files' when setting those variables may conflict with the
+;; customization of `org-agenda-files' itself.  Thus, when setting one of those
+;; variables would update `org-agenda-files' (possibly saving the customiztion),
+;; the original value of `org-agenda-files' would be gone.  Conversely, loading
+;; the customization for `org-agenda-files' would overwrite the work done by
+;; custom setters.  Thus, the only reasonable thing to do is to not update
+;; `org-agenda-files' automatically and leave it to the user to update it.
+
 (defcustom db/org-default-work-file nil
-  "Path to default org-mode file at work."
+  "Path to default org-mode file at work.
+You may also want to add this file to `org-agenda-files'."
   :group 'personal-settings
   :type '(choice (const nil) file))
 
 (defcustom db/org-default-home-file nil
-  "Path to default org-mode file at home."
+  "Path to default org-mode file at home.
+You may also want to add this file to `org-agenda-files'."
   :group 'personal-settings
   :type '(choice (const nil) file))
 
 (defcustom db/org-default-notes-file nil
-  "Path to default org-mode file for notes."
+  "Path to default org-mode file for notes.
+You may also want to add this file to `org-agenda-files'."
   :group 'personal-settings
   :type '(choice (const nil) file))
 
 (defcustom db/org-default-refile-file nil
-  "Path to default org-mode file for capturing."
+  "Path to default org-mode file for capturing.
+This file is used by `org-agenda' to query for tasks that need to
+be refiled, independently of whether it's part of
+`org-agenda-files' or not.  You may still want to add this file
+to `org-agenda-files' to have appointments, deadlines, etc shown
+in the main agenda view."
   :group 'personal-settings
   :type '(choice (const nil) file))
 
