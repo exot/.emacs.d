f4b55ad7774162b0f1080a5c49777499ef7b0b55
Daniel Borchmann <daniel@algebra20.de>
2020-01-11 10:13:30 +0100

    Add tests for NTP time conversion

---
+++ site-lisp/db-utils-test.el
@@ -0,0 +1,18 @@
+;;; db-utils-test.el -- Tests for db-utils -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+(require 'db-utils)
+(require 'cl-lib)
+
+(ert-deftest db-utils-db/ntp-to-time ()
+  "Test NTP conversion to readable time with `db/ntp-to-time'."
+  (should (equal (db/ntp-to-time #xdcd2ac0c #x05c6dbac)
+                 "2017-05-26T13:28:44.022565583Z"))
+  (should (equal (db/ntp-to-time #xbd5927ee #xbc616000)
+                 "2000-08-31T18:52:30.735860824Z")))
+
+;;; db-utils-test.el ends here
