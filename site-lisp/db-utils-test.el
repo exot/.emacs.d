c86051ba22e21caf8fc20c2474d7714935658862
Daniel Borchmann <daniel@algebra20.de>
2020-01-18 10:20:21 +0100

    Add obvious test for NTP conversion
    This test could not have been added before, because conversion was not
    guaranteed to work for dates before the unix epoch.  It now is, and we finally
    can check the start of the NTP epoch! :)

--- site-lisp/db-utils-test.el
+++ site-lisp/db-utils-test.el
@@ -13,6 +13,8 @@
   (should (equal (db/ntp-to-time #xdcd2ac0c #x05c6dbac)
                  "2017-05-26T13:28:44.022565583Z"))
   (should (equal (db/ntp-to-time #xbd5927ee #xbc616000)
-                 "2000-08-31T18:52:30.735860824Z")))
+                 "2000-08-31T18:52:30.735860824Z"))
+  (should (equal (db/ntp-to-time #x00000000 #x0000000)
+                 "1900-01-01T00:00:00.000000000Z")))
 
 ;;; db-utils-test.el ends here
