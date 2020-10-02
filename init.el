1d89551f1b4078dadc101b57c3afeab38a01f02f
Daniel Borchmann <daniel@algebra20.de>
2020-10-02 19:52:57 +0200

    Add missing autoload specification

--- init.el
+++ init.el
@@ -2404,7 +2404,8 @@ With given ARG, display files in `db/important-document-path’."
              db/playlist-files-from-git-annex-find
              db/play-auto-playlist-from-git-annex-find
              music-control/body
-             db/play-radio-stations))
+             db/play-radio-stations
+             db/update-playlist-files))
 
 
 ;; * Shells and such
