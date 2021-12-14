092cbd653d801712b715e0e5296c4d74e5b6610c
Daniel Borchmann <daniel@algebra20.de>
2021-12-14 19:34:34 +0100

    Ignore SOMEWHEN items on waiting-for list
    They are shown in the dedicated SOMEWHEN list, where they belong.

--- init.el
+++ init.el
@@ -1095,7 +1095,7 @@
                               (org-agenda-prefix-format '((tags . "%-12c %-4e ")))))))
 
                 ("U" "Unsupervised (Waiting, Missed Appointments, Hold)"
-                 ((tags-todo "WAIT-HOLD"
+                 ((tags-todo "WAIT-HOLD-SOMEWHEN"
                              ((org-agenda-overriding-header "Waiting For List")
                               (org-agenda-todo-ignore-scheduled t)
                               (org-agenda-sorting-strategy '(priority-down category-up))))
