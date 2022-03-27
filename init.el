ce9fa34c1a7c723203cdd7811223d7f81ea4c203
Daniel Borchmann <daniel@algebra20.de>
2022-03-27 09:44:35 +0200

    Include items in WIP list that are scheduled today or in the past
    This way, the WIP list faithfully shows all WIP items and gives a better
    overview of the current work load.  Items will appear twice though when they are
    scheduled today or in the past.

--- init.el
+++ init.el
@@ -1079,9 +1079,9 @@
                     (org-agenda-sorting-strategy '(deadline-up priority-down))
                     (org-deadline-warning-days 30)))
                   (tags-todo "TODO={CONT\\|ATTN}"
-                             ((org-agenda-overriding-header "WIP List (TODO={CONT\\\\|ATTN}, not scheduled)")
+                             ((org-agenda-overriding-header "WIP List (TODO={CONT\\\\|ATTN}, not scheduled in the future)")
                               (org-agenda-sorting-strategy '(priority-down category-up))
-                              (org-agenda-todo-ignore-scheduled t)))
+                              (org-agenda-todo-ignore-scheduled 'future)))
                   (tags-todo "TODO<>\"CONT\"-HOLD-SOMEWHEN-DATE-WAIT-TEMPLATE/-DONE"
                              ((org-agenda-overriding-header "Next Actions List (not WIP, not scheduled)")
                               (org-tags-match-list-sublevels t)
