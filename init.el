836429db1cd2fce2e37387d5fbeaacb251375763
Daniel Borchmann <daniel@algebra20.de>
2022-08-04 18:52:11 +0200

    Fix obsolete usage of `lexical-let`

--- init.el
+++ init.el
@@ -2061,15 +2061,15 @@ respectively."
               "Compare marked files in dired with ediff."
               ;; from: https://oremacs.com/2017/03/18/dired-ediff/
               (interactive)
-              (lexical-let ((files (dired-get-marked-files))
-                            (wnd (current-window-configuration)))
+              (let ((files (dired-get-marked-files))
+                    (wnd (current-window-configuration)))
                 (if (<= (length files) 2)
-                    (lexical-let ((file1 (car files))
-                                  (file2 (if (cdr files)
-                                             (cadr files)
-                                           (read-file-name
-                                            "file: "
-                                            (dired-dwim-target-directory)))))
+                    (let ((file1 (car files))
+                          (file2 (if (cdr files)
+                                     (cadr files)
+                                   (read-file-name
+                                    "file: "
+                                    (dired-dwim-target-directory)))))
                       (if (file-newer-than-file-p file1 file2)
                           (ediff-files file2 file1)
                         (ediff-files file1 file2))
