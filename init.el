1e262aef13bde5e6e1307b392126e44f1fe0cdf0
Daniel Borchmann <daniel@algebra20.de>
2023-04-26 16:54:04 +0200

    Allow `scroll-left` command

--- init.el
+++ init.el
@@ -652,6 +652,7 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
+(put 'scroll-left 'disabled nil)
 
 (use-package abbrev
   :init (setq save-abbrevs 'silently
