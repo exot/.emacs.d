4c0af67656fe2ea05e3fc0f11630462685c9c0e1
Daniel Borchmann <daniel@algebra20.de>
2023-08-13 10:56:23 +0200

    Update elpa packages

--- elpa/dash-20230714.723/dash-pkg.el
+++ elpa/dash-20230714.723/dash-pkg.el
@@ -1,6 +1,8 @@
-(define-package "dash" "20221013.836" "A modern list library for Emacs"
+(define-package "dash" "20230714.723" "A modern list library for Emacs"
   '((emacs "24"))
-  :commit "3df46d7d9fe74f52a661565888e4d31fd760f0df" :authors
+  :commit "f46268c75cb7c18361d3cee942cd4dc14a03aef4" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainers
   '(("Magnar Sveen" . "magnars@gmail.com"))
   :maintainer
   '("Magnar Sveen" . "magnars@gmail.com")
