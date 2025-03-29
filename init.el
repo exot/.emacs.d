7d0eb498e29be43dc8b64416901527fe912d1d8b
Daniel Borchmann <daniel@algebra20.de>
2025-03-29 17:15:15 +0100

    Check for spelling in comments
    Again inspired by https://github.com/purcell/emacs.d .

--- init.el
+++ init.el
@@ -1488,7 +1488,8 @@ Note that this workaround is incomplete, as explained in this comment."
             (add-hook 'prog-mode-hook 'electric-indent-local-mode)
             (add-hook 'prog-mode-hook 'hl-line-mode)
             (add-hook 'prog-mode-hook 'page-break-lines-mode)
-            (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)))
+            (add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
+            (add-hook 'prog-mode-hook 'flyspell-prog-mode)))
 
 (use-package ediff
   :init (setq ediff-diff-options "-w"
