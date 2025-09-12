0b80424a6d9b7d9646cbbe3d888725cb2469e1a8
Daniel Borchmann <daniel@algebra20.de>
2025-09-12 17:50:30 +0200

    Disable default key binding to toggle Org comments
    I don't need this and often enough hit the key by accident.

--- init.el
+++ init.el
@@ -743,7 +743,9 @@ split horizontally again, but this extra work should not matter much."
   :bind (:map org-mode-map
               ([remap org-return] . (lambda () (interactive) (org-return :indent)))
               ([remap org-clock-goto] . db/org-clock-goto-first-open-checkbox)
-              ([remap org-goto] . consult-org-heading))
+              ([remap org-goto] . consult-org-heading)
+              ("C-c ;" . nil)           ; Disable default binding for `org-toggle-comment'
+              )
   :autoload (org-get-todo-state
              org-entry-get)
   :commands (org-return)
